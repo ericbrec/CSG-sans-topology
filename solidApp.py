91be6c2c8394c962b0454ba64e5573c255650755
Eric Brechner <ericbrec@msn.com>
2024-02-08 10:28:44 -0800

    Prep for demo

--- solidApp.py
+++ solidApp.py
@@ -270,29 +270,38 @@ class solidApp(bspyApp):
 if __name__=='__main__':
     app = solidApp()
 
-    order = 3
-    knots = [0.0] * order + [1.0] * order
-    nCoef = len(knots) - order
-    spline = Spline(BspySpline(2, 3, (order, order), (nCoef, nCoef), (knots, knots), \
-        (((-1.0, -1.0, -1.0), (0.0, 0.0, 0.0), (1.0, 1.0, 1.0)), \
-        ((1.0, 0.0, 1.0), (0.0, -5.0, 0.0), (1.0, 0.0, 1.0)), \
-        ((-1.0, 0.0, 1.0), (-1.0, 0.0, 1.0), (-1.0, 0.0, 1.0)))))
-    spline.flip_normal()
-    cap = utils.hyperplane_axis_aligned(3, 1, 0.7, False)
-    paraboloid = Solid(3, False)
-    paraboloid.boundaries.append(Boundary(spline, utils.create_hypercube([0.5, 0.5], [0.5, 0.5])))
-    paraboloid.boundaries.append(Boundary(cap, utils.create_hypercube([1.0, 1.0], [0.0, 0.0])))
-    app.draw_solid(paraboloid, "paraboloid", np.array((0, 0, 1, 1),np.float32))
+    if True:
+        square = utils.create_hypercube([1,1], [0,0])
+        star = utils.create_star(2.0, [0.0, 0.0], 90.0*6.28/360.0)
+        extrudedSquare = utils.extrude_solid(square,[[-2,2,-4],[2,-2,4]])
+        extrudedStar = utils.extrude_solid(star,[[-2,-2,-4],[2,2,4]])
+        combined = extrudedStar.union(extrudedSquare)
+        app.draw_solid(combined)
+        app.mainloop()
+    else:
+        order = 3
+        knots = [0.0] * order + [1.0] * order
+        nCoef = len(knots) - order
+        spline = Spline(BspySpline(2, 3, (order, order), (nCoef, nCoef), (knots, knots), \
+            (((-1.0, -1.0, -1.0), (0.0, 0.0, 0.0), (1.0, 1.0, 1.0)), \
+            ((1.0, 0.0, 1.0), (0.0, -5.0, 0.0), (1.0, 0.0, 1.0)), \
+            ((-1.0, 0.0, 1.0), (-1.0, 0.0, 1.0), (-1.0, 0.0, 1.0)))))
+        spline.flip_normal()
+        cap = utils.hyperplane_axis_aligned(3, 1, 0.7, False)
+        paraboloid = Solid(3, False)
+        paraboloid.boundaries.append(Boundary(spline, utils.create_hypercube([0.5, 0.5], [0.5, 0.5])))
+        paraboloid.boundaries.append(Boundary(cap, utils.create_hypercube([1.0, 1.0], [0.0, 0.0])))
+        app.draw_solid(paraboloid, "paraboloid", np.array((0, 0, 1, 1),np.float32))
 
-    spline = Spline(spline.spline.copy())
-    spline.flip_normal()
-    cap = utils.hyperplane_axis_aligned(3, 1, 0.7, False)
-    paraboloid2 = Solid(3,False)
-    paraboloid2.boundaries.append(Boundary(spline, utils.create_hypercube([0.5, 0.5], [0.5, 0.5])))
-    paraboloid2.boundaries.append(Boundary(cap, utils.create_hypercube([1.0, 1.0], [0.0, 0.0])))
-    paraboloid2.translate(np.array((0.0, 0.5, 0.55)))
-    app.draw_solid(paraboloid2, "paraboloid2", np.array((0, 1, 0, 1),np.float32))
+        spline = Spline(spline.spline.copy())
+        spline.flip_normal()
+        cap = utils.hyperplane_axis_aligned(3, 1, 0.7, False)
+        paraboloid2 = Solid(3,False)
+        paraboloid2.boundaries.append(Boundary(spline, utils.create_hypercube([0.5, 0.5], [0.5, 0.5])))
+        paraboloid2.boundaries.append(Boundary(cap, utils.create_hypercube([1.0, 1.0], [0.0, 0.0])))
+        paraboloid2.translate(np.array((0.0, 0.5, 0.55)))
+        app.draw_solid(paraboloid2, "paraboloid2", np.array((0, 1, 0, 1),np.float32))
 
-    paraboloid3 = paraboloid.intersection(paraboloid2)
-    app.draw_solid(paraboloid3, "p * p2")
-    app.mainloop()
+        paraboloid3 = paraboloid * paraboloid2
+        app.draw_solid(paraboloid3, "p * p2")
+        app.mainloop()
